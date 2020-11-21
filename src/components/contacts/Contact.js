f9b5ddd2b71bc3478020c2003a52e364f2c1c68c
ehmusman <ehmusman@gmail.com>
2020-11-21 23:57:03 +0500

    state toggle is done

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -2,21 +2,37 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 class Contact extends Component {
+    state = {
+        showContactDetail: false
+    }
+
     render() {
         const { name, email, phone } = this.props.contact;
+        const { showContactDetail } = this.state;
         return (
             <div className='card card-body mb-3'>
                 <h1 className="card-heading">
-                    {name}
+                    {name} {" "}
+                    <i className="fas fa-sort-down" style={{ cursor: "pointer" }}
+                        onClick={() => this.setState({
+                            showContactDetail: !this.state.showContactDetail
+                        })}
+                    >
+                    </i>
                 </h1>
-                <ul className="list-group">
-                    <li className="list-group-item">
-                        phone: {phone}
-                    </li>
-                    <li className="list-group-item">
-                        email: {email}
-                    </li>
-                </ul>
+                {showContactDetail ?
+                    (
+                        <ul className="list-group">
+                            <li className="list-group-item">
+                                phone: {phone}
+                            </li>
+                            <li className="list-group-item">
+                                email: {email}
+                            </li>
+                        </ul>
+
+                    )
+                    : null}
 
             </div>
         )
