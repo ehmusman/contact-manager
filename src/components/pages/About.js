d60f3cc5707987e8bd885a6193636c8d839fef01
ehmusman <ehmusman@gmail.com>
2020-11-23 22:51:03 +0500

    about page is created

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <h3 className="display-3">About Contact Manager</h3>
+            <p className="lead">This is a simple app to manage contacts</p>
+            <p className="text-secondary">
+                latest version
+            </p>
+        </div>
+    )
+}
+
+export default About;
