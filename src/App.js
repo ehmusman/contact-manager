36f742420f3eb14de42d19fcdcfd9db6d5cde78e
ehmusman <ehmusman@gmail.com>
2020-11-23 22:02:11 +0500

    react router is installed

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react'
 import Header from './components/layout/Header'
 import Contacts from './components/contacts/Contacts'
 import AddForm from './components/layout/AddForm'
-
 import { Provider } from './Context'
+
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 class App extends Component {
