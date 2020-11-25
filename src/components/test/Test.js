cb9ac2d0a0463a65569baf7901b2162a3ae2d6e1
ehmusman <ehmusman@gmail.com>
2020-11-25 21:28:34 +0500

    first fetchind data is under process

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -9,9 +9,9 @@ class Test extends Component {
 
     componentDidMount() {
         // this is the second most popular lifecycle.
-        //  ehat is written here is fired off after the component mount
+        //  what is written here is fired off after the component mount
         console.log("componentDidMount...")
-        // here we make our http request to an API or backend or fetching date from within of your component and putting it into your component state or where ever we call it.
+        // here we make our http request to an API or backend or fetching data from within of your component and putting it into your component state or where ever we call it.
     }
     componentWillMount() {
         console.log("componentWillMount...")
